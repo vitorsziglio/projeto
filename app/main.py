3dc4956477f3210dc08bf4cafcaf9c63ce7c542e
vitorsziglio <vszgamer2608@gmail.com>
2024-08-07 00:43:06 -0300

    Atualização APP

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Projeto Desafio"}
+    return {"Projeto Desafio TESTE"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
