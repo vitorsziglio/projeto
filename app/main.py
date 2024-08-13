97fb4c26af0101459e165d07153566d7e4bdaa04
vitorsziglio <vszgamer2608@gmail.com>
2024-08-13 10:30:36 -0300

    atualização app

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ class Item(BaseModel):
 
 @app.get("/")
 def read_root():
-    return {"Projeto Desafio TESTE"}
+    return {"Projeto Desafio"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
